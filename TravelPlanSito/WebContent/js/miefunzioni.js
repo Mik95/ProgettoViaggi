5d7a21a333c2fac7f3290b46b565efad0898b5f1
Alessandro Bertoli <a.bertoli@itsrizzoli.it>
2018-02-13 15:15:54 +0100

    Sito web, in lavorazione

---
+++ TravelPlanSito/WebContent/js/miefunzioni.js
@@ -0,0 +1,16 @@
+$('#li1').click(function(){
+	document.getElementById('tpl').width=500;
+});
+
+$('#li2').click(function(){
+	document.getElementById('tpl').width=400;
+});
+
+function ingrandimento() {
+	document.getElementById('logobar').height=50;
+};
+
+function rimpicciolimento() {
+	document.getElementById('logobar').height=40;
+};
+

f203a6a21fa38da93846ad5501884f79c5f8a8ac
Francesco Meani <f.meani@itsrizzoli.it>
2018-03-19 14:47:00 +0100

    inserimento immagini progetto Android

---
+++ Progetto/app/src/main/java/applicazione/progetto/travelplan/FragmentInteractionListener.java
@@ -0,0 +1,11 @@
+package applicazione.progetto.travelplan;
+
+/**
+ * Created by FRANCESCO on 13/02/2018.
+ */
+
+public interface FragmentInteractionListener {
+    void onFragmentInteraction();
+
+
+}
